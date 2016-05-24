d480a955e88311a874606301f26862b353a5ae2f
Rohini Satheesh <rsathees@usc.edu>
2016-05-23 17:16:03 -0700

    Get single digit sum of digits of a number

--- Practice/src/com/practice/arrays/Arrays.java
+++ Practice/src/com/practice/arrays/Arrays.java
@@ -6,16 +6,18 @@ public class Arrays {
     public static void main(String args[]){
         int arr[] = {1,2,3,4};
         int window = 3;
-        getMovingAverage(arr, window);
+        getMovingAverageSum(arr, window);
+        System.out.println("Sum of digits of 14 is "+getOneDigitSum(14));
     }
 
     //Given an array of integers and a window size, calculate the average of all integers in the sliding window.
-    private static void getMovingAverage(int arr[], int window){
+    private static void getMovingAverageSum(int arr[], int window){
         if(window > arr.length){
             System.out.println("Invalid input");
             return;
         }
         int j = 0;
+        int averageSum = 0;
         for(int i = 0; i < arr.length; i++){
             int sum = 0;
             j = i + window - 1;
@@ -24,7 +26,13 @@ public class Arrays {
             for (int k = i; k <= j; k++){
                 sum += arr[k];
             }
-            System.out.println(sum/window);
+            averageSum += sum/window;
         }
+        System.out.println(averageSum);
+    }
+
+    //get 1 digit recursive sum of all digits in a number
+    private static int getOneDigitSum(int number){
+        return 1+((number-1)%9);
     }
 }
